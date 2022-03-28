d6015d9e605d1434f0947878e02497296e6e1b3b
David Huck <david.huck25@gmail.com>
2022-03-28 14:23:41 -0400

    Modified substring.rb

--- substring.rb
+++ substring.rb
@@ -3,7 +3,7 @@ def substring(str,library)
     # Puts letters in lowercase and scans for punctuation and removes them and puts string into an array
     str_array = str.downcase.scan(/[\w']+/)
     # combine string and library
-    combine = str_array + lib
+    combine = str_array + library
     # Finds matches, adds a counter, returns hash
     combine.reduce(Hash.new(0)) do |word, total|
       word.store(total, word[total] + 1)
